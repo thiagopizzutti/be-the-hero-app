03c50225f3060f03e450ad8bc4ae6addf95e92a2
Thiago Pizzutti <tpizzutti@gmail.com>
2020-03-24 17:40:06 -0300

    back-end done

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,34 @@
+const connection = require('../database/connection')
+const crypto = require('crypto')
+
+module.exports = {
+    async index(request, response) {
+        const ongs = await connection('ongs').select('*')
+
+        return response.json(ongs)
+    },
+
+    create(request, response) {
+        const {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        } = request.body
+        const id = crypto.randomBytes(4).toString('HEX')
+
+        connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        })
+
+        return response.json({
+            id
+        })
+    }
+}
